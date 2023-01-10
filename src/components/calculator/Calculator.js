eb233d81c21e156b9075f451d18ec74cd6adac6d
TiagoAlmeida13 <tyygo@live.com>
2023-01-10 10:58:10 -0300

    atualização da calculadora

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,88 +1,45 @@
 import React, { useState } from 'react';
 
 function Calculator() {
-    const [displayValue, setDisplayValue] = useState('0');
-    const [operator, setOperator] = useState(null);
-    const [operand, setOperand] = useState(null);
+    const [value, setValue] = useState('');
 
-    function handleNumberClick(event) {
-        const value = event.target.innerText;
-        setDisplayValue(displayValue === '0' ? value : displayValue + value);
-    }
-
-    function handleOperatorClick(event) {
-        const value = event.target.innerText;
-        setOperator(value);
-        setOperand(displayValue);
-        setDisplayValue('0');
-    }
-
-    function handleClearClick() {
-        setDisplayValue('0');
-        setOperator(null);
-        setOperand(null);
-    }
-
-    function handleEqualClick() {
-        if (operator && operand) {
-            const current = parseFloat(displayValue);
-            const previous = parseFloat(operand);
-            let result;
-
-            switch (operator) {
-                case '+':
-                    result = previous + current;
-                    break;
-                case '-':
-                    result = previous - current;
-                    break;
-                case '*':
-                    result = previous * current;
-                    break;
-                case '/':
-                    result = previous / current;
-                    break;
-                default:
-                    break;
-            }
-            setDisplayValue(result.toString());
-            setOperator(null);
-            setOperand(null);
+    function handleClick(e) {
+        const buttonValue = e.target.innerText;
+        if (buttonValue === 'C') {
+            setValue('');
+        } else if (buttonValue === '=') {
+            const result = eval(value);
+            setValue(result);
+        } else {
+            setValue(value + buttonValue);
         }
     }
 
     return (
         <div>
-            <div className="calculator-display">{displayValue}</div>
-            <div className="calculator-keypad">
-                <div className="input-keys">
-                    <div className="function-keys">
-                        <button className="calculator-key key-clear" onClick={handleClearClick}>AC</button>
-                        <button className="calculator-key key-sign">±</button>
-                        <button className="calculator-key key-percent">%</button>
-                    </div>
-                    <div className="digit-keys">
-                        <button className="calculator-key key-0" onClick={handleNumberClick}>0</button>
-                        <button className="calculator-key key-dot">.</button>
-                        <button className="calculator-key key-1" onClick={handleNumberClick}>1</button>
-                        <button className="calculator-key key-2" onClick={handleNumberClick}>2</button>
-                        <button className="calculator-key key-3" onClick={handleNumberClick}>3</button>
-                        <button className="calculator-key key-4" onClick={handleNumberClick}>4</button>
-                        <button className="calculator-key key-5" onClick={handleNumberClick}>5</button>
-                        <button className="calculator-key key-6" onClick={handleNumberClick}>6</button>
-                        <button className="calculator-key key-7" onClick={handleNumberClick}>7</button>
-                        <button className="calculator-key key-8" onClick={handleNumberClick}>8</button>
-                        <button className="calculator-key key-9" onClick={handleNumberClick}>9</button>
-                    </div>
-                </div>
-                <div className="operator-keys">
-                    <button className="calculator-key key-divide" onClick={handleOperatorClick}>/</button>
-                    <button className="calculator-key key-multiply" onClick={handleOperatorClick}>*</button>
-                    <button className="calculator-key key-subtract" onClick={handleOperatorClick}>-</button>
-                    <button className="calculator-key key-add" onClick={handleOperatorClick}>+</button>
-                    <button className="calculator-key key-equals" onClick={handleEqualClick}>=</button>
-                </div>
-            </div>
+            <input type="text" value={value} />
+            <br />
+            <button onClick={handleClick}>1</button>
+            <button onClick={handleClick}>2</button>
+            <button onClick={handleClick}>3</button>
+            <button onClick={handleClick}>+</button>
+            <br />
+            <button onClick={handleClick}>4</button>
+            <button onClick={handleClick}>5</button>
+            <button onClick={handleClick}>6</button>
+            <button onClick={handleClick}>-</button>
+            <br />
+            <button onClick={handleClick}>7</button>
+            <button onClick={handleClick}>8</button>
+            <button onClick={handleClick}>9</button>
+            <button onClick={handleClick}>*</button>
+            <br />
+            <button onClick={handleClick}>.</button>
+            <button onClick={handleClick}>0</button>
+            <button onClick={handleClick}>C</button>
+            <button onClick={handleClick}>/</button>
+            <br />
+            <button onClick={handleClick}>=</button>
         </div>
     );
 }
